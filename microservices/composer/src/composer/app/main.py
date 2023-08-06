0fa864dedc5429c004db42670703c8df09f78882
Domenico Di Gangi <digangidomenico@gmail.com>
2023-08-06 18:53:37 +0000

    black for formatting

--- microservices/composer/src/composer/app/main.py
+++ microservices/composer/src/composer/app/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from .api.api_v1 import router as api_v1_router
+
 
 app = FastAPI()
+
 
 @app.get("/")
 def read_root():
     return {"Hello": "FastAPI"}
+
+app.include_router(api_v1_router, prefix="/v1")
