ba3bc5a807b2ee8463a96e81b2eaa451a14d2802
Domenico Di Gangi <domenico.digangi@domotz.com>
2023-08-14 17:29:04 +0000

    progress

--- deploy/start/deploy_all.sh
+++ deploy/start/deploy_all.sh
@@ -1,7 +1,7 @@
  
 . deploy/kafka/strimzi_operator_based/deploy_strimzi_operator.sh 
 . deploy/kafka/strimzi_operator_based/deploy_kafka.sh 
-
+. deploy/kafka/helm_based/kafka-ui-deploy.sh
 . deploy/grafana/grafana-deploy-and-forward-ports.sh
 # run in grafana container
 # grafana cli plugins install hamedkarbasi93-kafka-datasource
