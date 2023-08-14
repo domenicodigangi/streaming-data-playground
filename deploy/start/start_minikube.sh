0880c60665fb7bbe0ae6493eb2376ecde4cf2fc5
Domenico Di Gangi <domenico.digangi@domotz.com>
2023-08-14 14:17:56 +0000

    updates for reprod startup

--- deploy/start/start_minikube.sh
+++ deploy/start/start_minikube.sh
@@ -6,6 +6,6 @@ if minikube status | grep -q "host: Running"; then
     echo "Minikube is already running."
 else
     echo "Minikube is not running. Starting Minikube..."
-    nohup bash -c 'minikube start --cpus=4 --memory=10GB --disk-size=10 &' > minikube.log 2>&1
+    minikube start --cpus=4 --memory=10GB --disk-size=10GB   > minikube.log 2>&1
 fi
  
