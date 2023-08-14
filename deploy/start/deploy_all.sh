dc3b5cf1c57537181f7b0db3c5d52f767f3ec39e
Domenico Di Gangi <domenico.digangi@domotz.com>
2023-08-14 17:10:54 +0000

    update script

--- deploy/start/deploy_all.sh
+++ deploy/start/deploy_all.sh
@@ -1,11 +1,12 @@
  
-. deploy/kafka/kafka-deploy.sh
+. deploy/kafka/strimzi_operator_based/deploy_strimzi_operator.sh 
+. deploy/kafka/strimzi_operator_based/deploy_kafka.sh 
 
 . deploy/grafana/grafana-deploy-and-forward-ports.sh
 # run in grafana container
 # grafana cli plugins install hamedkarbasi93-kafka-datasource
 
-kubectl apply -f deploy/kafka/kafka-client-producer.yml 
+# kubectl apply -f deploy/kafka/kafka-client-producer.yml 
 
 
 
