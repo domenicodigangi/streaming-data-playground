64226b15e30e760ac292d3fa5aadd045d0477a0e
Domenico Di Gangi <digangidomenico@gmail.com>
2023-08-15 08:48:37 +0000

    lighter on codespaces resources

--- deploy/start/start_minikube.sh
+++ deploy/start/start_minikube.sh
@@ -6,6 +6,6 @@ if minikube status | grep -q "host: Running"; then
     echo "Minikube is already running."
 else
     echo "Minikube is not running. Starting Minikube..."
-    minikube start --cpus=4 --memory=10GB --disk-size=10GB   > minikube.log 2>&1
+    minikube start --cpus=3 --memory=10GB --disk-size=10GB   > minikube.log 2>&1
 fi
  
