b1c33649d246bd1bf8a41cf703995e2948114fd3
Domenico Di Gangi <digangidomenico@gmail.com>
2023-08-16 08:33:21 +0000

    improove kafka setup

--- deploy/kafka/helm_based_ui/kafka-ui-deploy.sh
+++ deploy/kafka/helm_based_ui/kafka-ui-deploy.sh
@@ -8,11 +8,13 @@ NAMESPACE="kafka"
 KAFKA_UI_DEPLOYMENT_NAME="kafka-ui"
 KAFKA_TOPIC_NAME="topic-01"
 
+helm repo add kafka-ui https://provectus.github.io/kafka-ui-charts
  
 
 # Check if the service already exists
 if kubectl get svc -n $NAMESPACE | grep -q $KAFKA_UI_DEPLOYMENT_NAME; then
-    echo "Service '$KAFKA_UI_DEPLOYMENT_NAME' is already running."
+    echo "Service '$KAFKA_UI_DEPLOYMENT_NAME' is already running. Upgrade it"
+    helm upgrade $KAFKA_UI_DEPLOYMENT_NAME kafka-ui/kafka-ui --version 0.7.2 -f "$SCRIPT_DIR/kafka-ui-conf.yml" -n $NAMESPACE
 else
     echo "Service '$KAFKA_UI_DEPLOYMENT_NAME' is not running. Deploying..."
     helm install $KAFKA_UI_DEPLOYMENT_NAME kafka-ui/kafka-ui --version 0.7.2 -f "$SCRIPT_DIR/kafka-ui-conf.yml" -n $NAMESPACE
@@ -20,5 +22,6 @@ fi
 
 kubectl apply -f "$SCRIPT_DIR/kafka-ui-service.yml"
 # Set up port forwarding for kafka-ui
+lsof -t -i :3001 | xargs kill || true
 nohup kubectl port-forward -n $NAMESPACE deployment/$KAFKA_UI_DEPLOYMENT_NAME 3001:8080 || true &
  
--- deploy/kafka/strimzi_operator_based/deploy_kafka.sh
+++ deploy/kafka/strimzi_operator_based/deploy_kafka.sh
@@ -4,8 +4,13 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 echo "Running in $SCRIPT_DIR"
 
+. "$SCRIPT_DIR/deploy_strimzi_operator.sh"
+
 # Apply the Kafka configuration
-kubectl apply -f "$SCRIPT_DIR/kafka-persistent-single.yml" -n kafka
+kubectl apply -f "$SCRIPT_DIR/kafka-ephemeral-single.yml" -n kafka
 
 # Wait for the Kafka cluster to be ready
 kubectl wait kafka/cluster-01 --for=condition=Ready --timeout=300s -n kafka
+
+
+kubectl create -n kafka -f "$SCRIPT_DIR/topic-01.yml"
