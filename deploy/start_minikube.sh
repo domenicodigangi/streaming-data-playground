d7638de938bf9892a0db145474fc4b7990d3bfb7
Domenico Di Gangi <digangidomenico@gmail.com>
2023-08-29 17:14:47 +0000

    lower ram buffer

--- deploy/start_minikube.sh
+++ deploy/start_minikube.sh
@@ -6,7 +6,7 @@ available_cpus=$(nproc)
 available_memory=$(awk '/MemAvailable/ {printf "%.f", $2/1024/1024}' /proc/meminfo) # in GB
 
 # Subtract 2GB from available memory
-available_memory=$((available_memory - 1))
+available_memory=$((available_memory - 0))
 
 # Set maximum limit for Minikube resources
 max_cpus=3
@@ -18,7 +18,7 @@ final_memory=$(( available_memory < max_memory ? available_memory : max_memory )
 
 # Log the resources being used
 echo "Available CPUs: $available_cpus"
-echo "Available Memory: ${available_memory}GB (1GB subtracted)"
+echo "Available Memory: ${available_memory}GB (0GB subtracted)"
 echo "Allocating CPUs for Minikube: $final_cpus"
 echo "Allocating Memory for Minikube: ${final_memory}GB"
 
