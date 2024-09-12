3df7219954a78d719bd7544306e4a732824a51e2
Domenico Di Gangi <domenico.digangi@domotz.com>
2024-09-12 18:15:11 +0200

    fix simulator

--- components/streaming_anomaly_detection/data-simulator/src/simulator/core/data_generators/anomalies/__init__.py
+++ components/streaming_anomaly_detection/data-simulator/src/simulator/core/data_generators/anomalies/__init__.py
@@ -7,15 +7,14 @@ logger = logging.getLogger(__name__)
 
 
 class AnomalyDefinition(BaseModel):
-    standard_profile: conlist(float, min_length=20, max_length=20)
+    values: conlist(float, min_length=20, max_length=20)
     n_observations: int = Field(..., gt=0)
 
 
 class AnomalyHelper:
     def __init__(self, anomaly_definition: AnomalyDefinition):
-        self.standard_profile = anomaly_definition.standard_profile
+        self.values = anomaly_definition.values
         self.n_observations = anomaly_definition.n_observations
 
     def generate_list_to_add(self) -> list[float]:
-        return self.standard_profile  # TODO need to rescale to required n observations
-       
+        return self.values  # TODO need to rescale to required n observations
